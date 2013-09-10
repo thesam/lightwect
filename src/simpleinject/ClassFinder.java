0e1c95e65e7d47d39aa7d2fd04f4a5244d5b5fbe
Samuel <Samuel@130.239.213.119>
2013-09-10 19:11:22 +0200

    Can now find classes.

--- src/simpleinject/ClassFinder.java
+++ src/simpleinject/ClassFinder.java
@@ -1,24 +1,45 @@
 package simpleinject;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
 
 public class ClassFinder {
 
-	public List<Class> find() {
+	public List<Class> find() throws Exception {
 		String classpath = System.getProperty("java.class.path");
+		List<Class> classes = new ArrayList<>();
 		for (String classpathentry : classpath.split(";")) {
-			File dir = new File(classpathentry);
-			if (dir.isDirectory()) {
-				for (File file : dir.listFiles()) {
-					System.out.println(file);
+			classes.addAll(traverse(new File(classpathentry)));
+		}
+		return classes;
+	}
+
+	private List<Class> traverse(File classpathentry) throws Exception {
+		List<Class> classes = new ArrayList<>();
+		if (classpathentry.isDirectory()) {
+			for (File file : classpathentry.listFiles()) {
+				classes.addAll(traverse(file));
+			}
+		} else {
+			Package[] packages = Package.getPackages();
+			for (Package package1 : packages) {
+				try {
+					Class clazz = Class.forName(package1.getName()
+							+ "."
+							+ classpathentry.getName().replaceAll("\\.class",
+									""));
+					classes.add(clazz);
+				} catch (Exception e) {
 				}
 			}
 		}
-		return new ArrayList<>();
+		return classes;
 	}
 
 }
--- test/simpleinject/ClassFinderTest.java
+++ test/simpleinject/ClassFinderTest.java
@@ -7,11 +7,15 @@ import java.util.List;
 import org.junit.Test;
 
 public class ClassFinderTest {
+	
+	public class A {
+		
+	}
 	@Test
-	public void testName() throws Exception {
+	public void canFindClassesOnClasspath() throws Exception {
 		ClassFinder finder = new ClassFinder();
 		List<Class> find = finder.find();
-		assertTrue(find.size() > 0);
+		assertTrue(find.contains(A.class));
 	}
 
 }
